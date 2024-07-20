e0431f5bc4f8a5123189f1121e0cc6b9059fba3f
Ghonim <asghonim@live.com>
2024-07-20 18:05:48 +0300

    assignment

--- hello-ahmed.js
+++ hello-ahmed.js
@@ -1,4 +1,9 @@
-x = 5;
-y = 10;
-z = x + y;
-console.log("The output is", z);
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// create two empty arrays, one is called "odd"
+// and the other is called "even"
+
+// loop over the input array
+// if the item is event (i.e. divisible by 2)
+// put it in the even array
+// if the item is odd (i.e. not divisible by 2)
+// put it in the odd array

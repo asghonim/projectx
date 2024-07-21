c6da6619d6c644a080b5660b4c6a1762e347a85d
yasser.abdelhakiem626@gmail.com <yasser.abdelhakiem626@gmail.com>
2024-07-21 11:58:02 +0300

    Assignment solution

--- hello-ammar.js
+++ hello-ammar.js
@@ -1,4 +1,18 @@
-x = 5;
-y = 10;
-z = x + y;
-console.log("The Output Is ", z);
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// The length property of an array returns the length of an array 
+// The easiest way to add a new element to an array is using the push() method:
+
+const even = [];
+const odd = [];
+let inputLen = input.length;
+
+for (i = 0; i < inputLen; i++) {
+    if (input[i] % 2 == 0) {
+        even.push(input[i]);
+    } else {
+        odd.push(input[i]);
+    }
+}
+
+console.log("Even Numbers: ", even);
+console.log("Odd Numbers: ", odd);

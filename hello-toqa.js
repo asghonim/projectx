77e2e470edd593bb0e495fa8c7753e8974b0cb2e
Toqa saber <sabertoqa720@gmail.com>
2024-07-23 14:08:12 +0300

    hello-toqa.js

--- hello-toqa.js
+++ hello-toqa.js
@@ -1,5 +1,14 @@
-x = 5;
-y = 10;
-z = x+y;
-console.log("the output is", z);
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const odd = [];
+const even = [];
 
+for (let i = 0; i < input.length; i++) {
+  if (input[i] % 2 == 0) {
+    even.push(input[i]);
+  } 
+  else {
+    odd.push(input[i]);
+  }
+}
+console.log("Odd numbers:", odd);
+console.log("Even numbers:", even);
